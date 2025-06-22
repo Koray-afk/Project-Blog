f04c666bcc1349b5bfec7f01340332c5c9f86db7
Koray-afk <abhiman.singh2024@nst.rishihood.edu.in>
2025-06-22 14:56:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return ( 
+    <>
+     Creating a blog app 
+    </>
+  )
+}
+
+export default App
